4d5e7b51e30c1f9f56e5cadd4ea30fc6976b64b0
Subhransu Dhal <subhransudhal694@gmail.com>
2024-05-29 21:00:11 +0530

    export

---
+++ Module1/mainmodule.js
@@ -0,0 +1,8 @@
+
+
+const aritmodule = require("./arithmaticmodule");
+
+console.log("add: "+aritmodule.add(5,2));
+console.log("sub: "+aritmodule.sub(6,2));
+console.log("mul: "+aritmodule.mul(1,2));
+console.log("div: "+aritmodule.div(6,2));
